ba213488321409ec03c053ad7ab48a205aff57bd
Harry <harry@fakku.net>
2018-01-24 22:15:54 -0800

    fixing pep8 formatting

--- getPricing.py
+++ getPricing.py
@@ -3,10 +3,12 @@ import json
 
 AuthSettings = json.load(open('auth.json'))
 
+
 def getAuthToken(user, pw):
-  #print(str(user))
-  #print(str(pw))
-  r = requests.post('https://www.echomtg.com/api/user/auth/', data={'email':str(user),'password':str(pw)})
+  # print(str(user))
+  # print(str(pw))
+  r = requests.post('https://www.echomtg.com/api/user/auth/',
+                    data={'email': str(user), 'password': str(pw)})
   responseBody = json.loads(r.text)
   if responseBody['status'] == 'success':
     return responseBody['token']
@@ -14,9 +16,11 @@ def getAuthToken(user, pw):
     print('Authentication error')
     print(r.text)
 
+
 def buildCardList(token):
-  cardRequest = requests.get('https://www.echomtg.com/api/stores/card_reference/auth=' + str(token))
-  #print(cardRequest.text)
+  cardRequest = requests.get(
+      'https://www.echomtg.com/api/stores/card_reference/auth=' + str(token))
+  # print(cardRequest.text)
   cardResponse = json.loads(cardRequest.text)
   cardList = cardResponse['cards']
   cardFile = open('cardList.json', 'w')
